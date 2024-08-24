668919cf90e80b77683ce02bcd347a6e3a331dae
Negar <n.arasterad@gmail.com>
2024-08-24 17:01:15 +0330

    gradient change

--- src/app/_Components/Footer.js
+++ src/app/_Components/Footer.js
@@ -16,7 +16,12 @@ const Footer = () => {
       className="w-full h-full md:rounded-tr-full rounded-tr-[15rem]"
       style={{
         background:
-          "linear-gradient(90deg, hsla(210, 100%, 25%, 1) 8%, hsla(210, 16%, 93%, 1) 100%)",
+          "radial-gradient(circle at center top, hsla(210, 16%, 93%, 1) 8%, hsla(210, 100%, 25%, 1) 100%)",
+        
+        "@media (max-width: 768px)": {
+          background:
+            "radial-gradient(circle at center top, hsla(210, 16%, 93%, 1) 40%, hsla(210, 100%, 25%, 1) 100%)",
+        },
       }}
     >
       {/* Top Desktop Content */}
@@ -29,20 +34,20 @@ const Footer = () => {
         </div>
 
         <ul className="basis-1/3">
-          <h6 className="text-white font-semibold mb-4 text-center pb-4 m-auto max-w-[40%] relative">
+          <h6 className="text-blue font-semibold mb-4 text-center pb-4 m-auto max-w-[40%] relative">
             تماس با ما <div className="w-full h-1 bg-blue rounded-sm absolute top-9 left-0 overflow-hidden"><span className="w-6 h-full bg-white rounded-sm absolute top-0 left-2 animation-border"></span></div>
           </h6>
-          <li className="flex justify-center items-center gap-3 mb-4 mt-4 text-sm">
+          <li className="flex justify-center items-center gap-3 mb-4 mt-4 text-sm font-semibold">
             <PhoneEnabled className="text-blue" />
-            <p className="text-white">021-91008802</p>
+            <p className="text-blue">021-91008802</p>
           </li>
-          <li className="flex justify-center items-center gap-3 mb-4 text-sm">
+          <li className="flex justify-center items-center gap-3 mb-4 text-sm font-semibold">
             <Email className="text-blue" />
-            <p className="text-white">info@artemist.com</p>
+            <p className="text-blue">info@artemist.com</p>
           </li>
-          <li className="flex justify-center items-center mt-4 mx-5 text-sm">
+          <li className="flex justify-center items-center mt-4 mx-5 text-sm font-semibold">
             <Place className="text-blue" />
-            <p className="text-white text-center">
+            <p className="text-blue text-center">
               کرج, چهارراه هفت تیر, ابتدای بلوار بلال, بن بست نادری, ساختمان
               کریستال, واحد 2, طبقه دوم
             </p>
@@ -50,19 +55,19 @@ const Footer = () => {
         </ul>
 
         <ul className="text-center basis-1/3">
-          <h6 className="text-white font-semibold mb-4 pb-4 text-center m-auto max-w-[40%] relative">
+          <h6 className="text-blue font-semibold mb-4 pb-4 text-center m-auto max-w-[40%] relative">
             لینک های مهم <div className="w-full h-1 bg-blue rounded-sm absolute top-9 left-0 overflow-hidden"><span className="w-6 h-full bg-white rounded-sm absolute top-0 left-2 animation-border"></span></div>
           </h6>
-          <li className="mb-4 text-white mt-4 text-sm">
+          <li className="mb-4 text-blue mt-4 text-sm font-semibold">
             <Link href="/">صفحه اصلی</Link>
           </li>
-          <li className="mb-4 text-white text-sm">
+          <li className="mb-4 text-blue text-sm font-semibold">
             <Link href="/about">درباره ما</Link>
           </li>
-          <li className="mb-4 text-white text-sm">
+          <li className="mb-4 text-blue text-sm font-semibold">
             <Link href="/automation">اتوماسیون اداری</Link>
           </li>
-          <li className="mt-4 text-white text-sm">
+          <li className="mt-4 text-blue text-sm font-semibold">
             <Link href="/guidance">سامانه راهنمایان آرتمیس</Link>
           </li>
         </ul>
@@ -71,7 +76,7 @@ const Footer = () => {
       {/* Top Mobile Content */}
       <div className="w-full h-full flex md:hidden flex-col md:flex-row justify-between max-w-[80%] mx-auto items-stretch pt-8 pb-7">
         <div className="flex flex-col justify-center items-center basis-1/3">
-          <Image src={logoImage1} alt="logoImage1" className="w-20" />
+          <Image src={logoImage1} alt="logoImage1" className="w-20 animation-pulse" />
           <p className="text-blue py-3 font-semibold text-center">
             شرکت آرتمیس <br /> کارگزار رسمی تور های خارجی
           </p>
@@ -168,9 +173,9 @@ const Footer = () => {
       </div>
 
       {/* Bottom Content */}
-      <hr className="w-[80%] mx-auto  border-b-1 border-blue" />
+      <hr className="w-[80%] mx-auto  border-b-1 border-white md:border-blue" />
       <div>
-        <p className="text-white text-center py-3 text-sm">
+        <p className="text-white md:text-blue text-center py-3 text-sm">
           کلیه حقوق این وب‌سایت متعلق به شرکت آرتمیس است. © ۱۴۰۳
         </p>
       </div>
