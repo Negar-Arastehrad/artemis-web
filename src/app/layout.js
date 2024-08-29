59f3f1a01f7d26b63b1e766221b9e1fd47290426
Negar <n.arasterad@gmail.com>
2024-08-29 21:39:28 +0330

    .

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,7 +17,7 @@ const figtree = Figtree({
 export const champagneLimousines = localFont({
   src: [
     {
-      path: "../../public/fonts/ttf/champagne-limousines-bold.ttf",
+      path: "../../public/fonts/ttf/champagnelimousinesbold.ttf",
       weight: "700",
       style: "normal",
     },
